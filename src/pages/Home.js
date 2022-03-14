d5e56f7d00b38ee430c057add077bdb3943ba23f
MarcelDurganteDev <info@marceldurgante.dev>
2022-03-14 02:20:57 +0100

    bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import { NavBar } from '../components/NavBar'
+import Announcement from '../components/Announcement';
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Slider from '../components/Slider';
+import Categories from '../components/Categories';
+import Products from '../components/Products';
 
-export default function Home() {
-  return (
-    <div><NavBar  /></div>
-  )
-}
+export const Home = () => {
+    return (
+        <div>
+            <Announcement />
+            <NavBar />
+            <Slider />
+            <Products />
+            <Categories />
+        </div>
+    );
+};
